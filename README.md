8fd0de49884cc8f43951af9af14378ced1c67d93
Jonathan MARTIN <j.martin@lonsdale.fr>
2021-06-30 18:19:26 +0200

    first

---
+++ README.md
@@ -0,0 +1,115 @@
+
+# Carousel
+
+![version](https://img.shields.io/github/manifest-json/v/Natjo/slider)
+
+Light carousel
+
+## Parameters
+| Parameter | Type | Default | Description |
+| ------ | ------ | ------ | ------ |
+|  |  | - |  |
+
+
+## Usage
+
+### Slider full
+
+#### html
+```html
+<div class="slider-full">
+	<div class="slider-container">
+		<ul class="slider">
+			<li>
+				<div class="item">
+					<img loading="lazy" src="https://picsum.photos/id/157/536/354" alt="">
+				</div>
+			</li>
+			<li>
+				<div class="item">
+					<img loading="lazy" src="https://picsum.photos/id/845/536/354" alt="">
+				</div>
+			</li>
+			<li>
+				<div class="item">
+					<img loading="lazy" src="https://picsum.photos/id/885/536/354" alt="">
+				</div>
+			</li>
+			<li>
+				<div class="item">
+					<img loading="lazy" src="https://picsum.photos/id/825/536/354" alt="">
+				</div>
+			</li>
+			<li>
+				<div class="item">
+					<img loading="lazy" src="https://picsum.photos/id/145/536/354" alt="">
+				</div>
+			</li>
+			<li>
+				<div class="item">
+					<img loading="lazy" src="https://picsum.photos/id/55/536/354" alt="">
+				</div>
+			</li>
+			<li>
+				<div class="item">
+					<img loading="lazy" src="https://picsum.photos/id/88/536/354" alt="">
+				</div>
+			</li>
+		</ul>
+	</div>
+</div>
+```
+#### javascript
+```javascript
+const slider_full = document.querySelector('.slider-full');
+const slider_full_slider = new Slider(slider_full);
+slider_full_slider.create();
+```
+
+### Slider
+
+#### html
+```html
+
+```
+#### javascript
+```javascript
+
+```
+#### Css
+```css
+
+```
+
+### Carousel
+
+#### html
+```html
+
+```
+#### javascript
+```javascript
+
+```
+#### Css
+```css
+
+```
+
+### Carousel full
+
+#### html
+```html
+
+```
+#### javascript
+```javascript
+
+```
+#### Css
+```css
+
+```
+
+## Demo
+[See codepen demo](https://codepen.io/natjo/pen/VwpBjWE)
---
+++ slider.js
@@ -0,0 +1,121 @@
+/**
+ * @module Slider
+ * 
+ */
+
+ function Slider(container){
+	const slider = container.querySelector('.slider');
+	const isTouch = 'ontouchstart' in document.documentElement;
+	const items = slider.querySelectorAll('li');
+	const total = items.length;	
+	let itemW, gap, nb;
+	let startX = 0, moveX = 0;
+	let dir;
+	let posNum = 0;
+	let posX = 0, oldposX;
+	let isMove = false;
+	
+	slider.style.setProperty('--total', total);
+	
+	const observer = new IntersectionObserver(entries => {
+		entries.forEach(e => {
+			if(e.isIntersecting){
+				e.target.removeAttribute('data-hidden');
+				e.target.style.visibility = 'visible';
+			}else{
+				e.target.setAttribute('data-hidden', true);
+				e.target.style.visibility = 'hidden';
+			}  
+		});
+	});
+	items.forEach((item,i) => observer.observe(item));
+	
+	const goto = (value, transition = true) => {
+		posX = -value * (itemW + gap);
+		if(transition) slider.style.transition = 'transform .4s ease';
+		slider.style.transform = `translate3d(${posX}px,0,0)`;
+	}
+	const resize = () => {
+		gap = parseInt(getComputedStyle(slider).gridColumnGap);
+		nb = parseInt(getComputedStyle(slider).getPropertyValue('--nb')) || 1;
+		itemW = items[0].clientWidth;
+		goto(posNum, false);
+	}
+	const mouseDown = value => {
+		moveX = value;
+		startX = value - posX;
+		slider.style.transition = 'none';
+		window.addEventListener('mouseup', clickout);
+	}
+	const mouseMove = value => {
+		posX = value - startX;
+		if(Math.abs(moveX - value) > 5 && !isMove){
+			slider.classList.add('onswipe');
+			isMove = true;
+			if(isTouch) document.body.classList.add('disableScroll');
+		}
+		slider.style.transform = `translate3d(${posX}px,0,0)`;
+		if(dir > value) posNum = -(posX - (itemW - 20)) / (itemW + gap);
+		else if(dir < value) posNum = -(posX - 20 - gap) / (itemW + gap);
+		dir = value;
+		oldposX = posX;
+	}
+	const mouseUp = () => {
+		slider.onmousemove = null;
+		slider.onmouseup = null;
+		posNum = Math.floor(posNum);
+		if(posNum < 0) posNum = 0;
+		if(posNum > total - nb) posNum = total - nb;
+		goto(posNum);
+		window.removeEventListener('mouseup', clickout);
+		slider.classList.remove('onswipe');
+		isMove = false;
+		document.body.classList.remove('disableScroll');
+	}
+	const leave = (e) => {
+		if(e.clientY <= 0 || e.clientX <= 0 || (e.clientX >= window.innerWidth || e.clientY >= window.innerHeight)) mouseUp();
+	}
+	const clickout = e => !slider.contains(e.target) && mouseUp();
+	
+	this.prev = () => {
+		posNum > 0 && posNum--;
+		goto(posNum);
+	}
+	this.next = () => {
+		posNum < total - nb && posNum++;
+		goto(posNum);
+	}
+	this.create = () => {
+		if(isTouch){
+			slider.ontouchstart = e => {
+				mouseDown(e.touches[0].clientX);
+				slider.ontouchmove = e => mouseMove(e.touches[0].clientX);
+				slider.ontouchend = e => mouseUp(e.changedTouches[0].clientX);
+			}
+		}
+		else{
+			slider.onmousedown = e => {
+				mouseDown(e.clientX);
+				slider.onmousemove = e => mouseMove(e.clientX);
+				slider.onmouseup = e => mouseUp(e.clientX);
+				return false;
+			}
+			window.addEventListener('resize', resize, {passive: true});
+			document.querySelector('html').addEventListener('mouseleave', leave);
+		}
+	}
+	this.destroy = () => {
+		slider.onmousemove = null;
+		slider.onmouseup = null;
+		slider.ontouchstart = null;
+		slider.onmousedown = null;
+		slider.style.transition = 'none';
+		slider.style.transform = 'none';
+		window.removeEventListener('resize', resize);
+		document.querySelector('html').removeEventListener('mouseleave', leave);
+	}
+		
+	resize();
+}
+
+export default Slider;
